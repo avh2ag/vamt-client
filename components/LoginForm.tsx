2ef3358ba4697b475a7196f1c3b99c17a3cbb81b
alex harasty <alex.harasty@gmail.com>
2023-07-15 22:07:22 -0400

    add redirect behavior to home page after succesful login

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { login } from '../utils/token';
 
 const LoginForm: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    await login(username, password);
-    // handle response data
+    try {
+      // only avoids error if 200 and has token
+      await login(username, password);
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.push('/'); // change this to your desired route
+    }
+  }, [isLoggedIn, router]);
 
   return (
     <form onSubmit={handleSubmit}>
